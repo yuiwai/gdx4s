6334a3a990540dbaad28c814c6d4688fe02cd034
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2020-01-02 11:17:15 +0900

    EventHandlerのボタン抽出処理を修正

--- core/src/main/scala/com/yuiwai/gdxs/event/EventHandler.scala
+++ core/src/main/scala/com/yuiwai/gdxs/event/EventHandler.scala
@@ -2,7 +2,7 @@ package com.yuiwai.gdxs.event
 
 import com.badlogic.gdx.InputProcessor
 import com.yuiwai.gdxs.{Pos, Region}
-import com.yuiwai.gdxs.component.Button
+import com.yuiwai.gdxs.component.{Button, Component, Layout}
 import com.yuiwai.gdxs.view.ViewProfile
 
 trait EventHandler[A] extends InputProcessor {
@@ -13,13 +13,15 @@ trait EventHandler[A] extends InputProcessor {
       .camera
       .unproject(pos.toVector3)
   }
-  lazy val buttons: Seq[(Button[A], Region)] =
-    viewProfile.components.filter {
-      _.cell.isInstanceOf[Button[A]]
-    }
-      .map { c => c.cell.asInstanceOf[Button[A]] -> c.region }
-
-  // FIXME 暫定ボタン実装
+  lazy val buttons: Seq[(Button[A], Region)] = findButtons(viewProfile.components)
+  private def findButtons(components: Seq[Component]): Seq[(Button[A], Region)] =
+    components
+      .map(c =>  c -> c.cell)
+      .flatMap {
+        case (c, b: Button[A]) => Seq(b -> c.region)
+        case (_, l: Layout) => findButtons(l.children)
+        case _ => Seq.empty
+      }
   override def touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = {
     val pos = screenToLocal(Pos(screenX, screenY))
     buttons.find(_._2.area.hitTest(pos)) match {
